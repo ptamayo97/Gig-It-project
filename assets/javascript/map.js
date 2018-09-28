86f6877661252c029337f2de116e2fbf8cf4fb40
Tim Westberg <timwestberg43@gmail.com>
2018-09-28 01:30:14 -0700

    Added some spacing to the index.html along with also added in stylesheets in correctly so i can keep working on maps, im done with this file for awhile wont need it

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,9 +1,9 @@
 function initMap() {
 
-    let venue = {lat:32.7157 , lng:1-117.1628};
+    let venue = {lat:32.7157 , lng:-117.1628};
 
     let map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: venue});
+        document.getElementById('map'), {zoom: 12, center: venue});
 
     let marker = new google.maps.Marker({position: venue, map:map});
     
