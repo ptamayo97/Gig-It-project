eed8e6cd6a2212a014295a2be679e6cd9f444c48
Entreri <tphakasoum@gmail.com>
2018-09-29 11:10:34 -0700

    creating separate js file for bandsintown and deleted code from app.js. Updated index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,19 +10,4 @@ var config = {
   };
   firebase.initializeApp(config);
  
- //displays artist's information from bandsintown API 
- function displayArtistInfo(){
-   var artist 
-   var date 
-   var queryURL = "https://rest.bandsintown.com/artists/"+artist+"/events?app_id=3c23ec0eb335a5c10b8f6691c2121940&date="+date;
-   $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-  console.log(response);
-  
-  })
- }
- //call back function to display JSON for artist and events
- displayArtistInfo()
 
