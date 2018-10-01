f4c2f9c1dcedc403cc7b6bb6dc313fed052305f0
Entreri <tphakasoum@gmail.com>
2018-09-30 21:20:58 -0700

    updated bands.js to target selection from dates drop list. Added code to display information of events and artist infor to the DOM. Still working on  date ranges and display info to the dome

--- assets/javascript/bands.js
+++ assets/javascript/bands.js
@@ -1,39 +1,75 @@
- //displays artist's information from bandsintown API 
- function displayArtistInfo(artist,date){
-   //code to grab drop selection dates and create code: for today, 7 days, and month then displays events;
-   //if today is selected then date variable will change to today or upcoming
-   //if 7 days is selected then date varialbe will equal to dange range of 7 days
-   //if this month is selected then date varialbe will equal to current month
-   //link drop selection to date variable
-   //convert date variable to api parameters
-   //link val in search form to artist variable
-   var artist
-   var date
-    var queryURL = "https://rest.bandsintown.com/artists/"+artist+"/events?app_id=3c23ec0eb335a5c10b8f6691c2121940&date="+date;
-    $.ajax({
-     url: queryURL,
-     method: "GET"
-   }).then(function(response) {
- //ajax to assess object value to display on map Venue location(latitude and physical address),
- for(i = 0; i < response.length; i++){
-  //console log venue, city, lat and long) 
-  console.log("Venue: "+response[i].venue.name);
-  console.log("City: "+response[i].venue.city);
-  console.log("Latitude: "+response[i].venue.latitude);
-  console.log("Longitude: "+response[i].venue.longitude);
-  var dateEvent = response[i].datetime;
-  console.log(dateEvent);
-  var prettyDateEvent = moment(dateEvent).format("MM/DD/YY @ hh:mm a");
-  //console log date and time of the vent
-  console.log(prettyDateEvent);
-  //ajax to assess: top 5 tracks, photos(members, events, album art, anything else you might think would be cool) dates 
-  var artistInfo = response[i].url;
-  console.log(artistInfo);
-  console.log("--------------------------------------");
+//call back function to display JSON for artist and events
+function displayArtistEvents(artist, date) {
+  //bands in town API
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=3c23ec0eb335a5c10b8f6691c2121940&date=" + date;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    //displays artist page from bandinstown
+    var artistInfo = response[0].url;
+    var a = $("<a>").text("Artist's Website");
+    var aOne = a.attr("href", artistInfo);
+    $(".artistPage").append(aOne);//need to create div class "artistPage" in html for where artist page will display
+    //ajax to assess object value to display on map Venue location(latitude and physical address),
+    for (i = 0; i < response.length; i++) {
+      //console log venue, city, lat and long)
+      var venue = response[i].venue.name;
+      var city = response[i].venue.city;
+      var latitude = response[i].venue.latitude;
+      var longitude = response[i].venue.longitude;
+      var dateEvent = response[i].datetime;
+      var prettyDateEvent = moment(dateEvent).format("MM/DD/YY @ hh:mm a");
+      var artist = $("<div>");
+      var v = $("<p>").text(venue);
+      artist.append(v);
+      var c = $("<p>").text("City: " + city);
+      artist.append(c);
+      var d = $("<p>").text("Date: " + prettyDateEvent);
+      artist.append(d);
+      $(".artistInfo").append(artist);
+      console.log("Venue: " + venue);
+      console.log("Latitude: " + latitude);
+      console.log("Longitude: " + longitude);
+      console.log("--------------------------------------");
+    }
+  })
+};
 
- }
-   })
-  
+function displayArtistInfo(artist) {
+  //bands in town API
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=3c23ec0eb335a5c10b8f6691c2121940";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    $(".artistName").append(response.name);
+    //append facebook page to html
+    //append image to html
+    //append bandsintown page to html
+    //append number of upcoming events
+  })
+  }    
+//code to grab drop selection dates and create code: for today, 7 days, and month then displays events;
+//need to create submit button in html
+$("#submit").on("click", function () {
+  //add jquery for return button
+  artist = $("#search").val().toLowerCase();
+    displayArtistInfo(artist);
+  //if 7 days is selected then date varialbe will equal to dange range of 7 days
+  if ($("#timeFrame").val() === "thisWeek") {
+    date //convert date variable to api parameters
+    //if this month is selected then date varialbe will equal to current month
+  } else if ($("#timeFrame").val() === "thisMonth") {
+    date  //link drop selection to date variable
+  } else {
+    date = "upcoming" //if today is selected then date variable will change to today or upcoming
   }
-  //call back function to display JSON for artist and events
-  displayArtistInfo("maroon5","upcoming");
+  displayArtistEvents(artist, date);
+  $("#search").val("");
+  //div class from html
+  $(".artistPage").empty();
+  $(".artistName").empty();
+  $(".artistInfo").empty();
+});
