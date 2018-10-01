422080f25c2c1c4f4608550bddc9491625395cc1
Entreri <tphakasoum@gmail.com>
2018-10-01 13:18:58 -0700

    added option to only pull artist information such as bandsintown page and fb page, and to pull picture

--- assets/javascript/bands.js
+++ assets/javascript/bands.js
@@ -44,32 +44,52 @@ function displayArtistInfo(artist) {
     method: "GET"
   }).then(function (response) {
     console.log(response);
-    $(".artistName").append(response.name);
+    var banDiv = $("<div>");
+    var bandName = response.name;
+    banDiv.append(bandName);
     //append facebook page to html
+    var banFb = response.facebook_page_url;
+    var banDiv2 = $("<div>");
+    var aOne = $("<a>").text("Artist's Facebook Page").attr("href", banFb);
+    banDiv2.append(aOne);
     //append image to html
+    var bImage = $("<img>").attr("src",response.image_url);
     //append bandsintown page to html
+    var banBit = response.url;
+    var banDiv3 = $("<div>");
+    var aOne = $("<a>").text("Artist's BandsInTown Page").attr("href", banBit);
+    banDiv3.append(aOne);
+    var bImage2 = $("<img>").attr("src",response.thumb_url);
     //append number of upcoming events
+
+    $(".artistName").append(banDiv, bImage2, banDiv2, banDiv3);
+
   })
   }    
 //code to grab drop selection dates and create code: for today, 7 days, and month then displays events;
 //need to create submit button in html
 $("#submit").on("click", function () {
+  $(".artistName").empty();
+  $(".artistPage").empty();
+  $(".artistInfo").empty();
   //add jquery for return button
   artist = $("#search").val().toLowerCase();
-    displayArtistInfo(artist);
+    //displayArtistInfo(artist);
   //if 7 days is selected then date varialbe will equal to dange range of 7 days
   if ($("#timeFrame").val() === "thisWeek") {
     date //convert date variable to api parameters
+    displayArtistEvents(artist, date)
     //if this month is selected then date varialbe will equal to current month
   } else if ($("#timeFrame").val() === "thisMonth") {
     date  //link drop selection to date variable
-  } else {
+    displayArtistEvents(artist, date)
+  } else if ($("#timeFrame").val()==="today"){
     date = "upcoming" //if today is selected then date variable will change to today or upcoming
+    displayArtistEvents(artist, date)
+  } else{
+    displayArtistInfo(artist);
   }
-  displayArtistEvents(artist, date);
+  ;
   $("#search").val("");
-  //div class from html
-  $(".artistPage").empty();
-  $(".artistName").empty();
-  $(".artistInfo").empty();
+ 
 });
