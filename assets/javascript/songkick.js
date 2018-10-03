e9fdfeda0fe154acbae48a8003602de1bf7afc8e
sharoncalifornia <sharonzhang.california@gmail.com>
2018-10-02 18:37:51 -0700

    modified songkick.js

--- assets/javascript/songkick.js
+++ assets/javascript/songkick.js
@@ -1,22 +1,35 @@
 //search by artist
+var artist_id;
+
 function search_by_artist(artist_name){
 
     var queryURL="https://api.songkick.com/api/3.0/search/artists.json?apikey=4s4kGBz0WjJ4Yk6O&query="+artist_name;
-   
+    
+    var temp_id = "";
    
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
       //console.log(response);
+      
       console.log(response.resultsPage.results);
       console.log("artist id: "+response.resultsPage.results.artist[0].id);
-      
+     
+      artist_id=response.resultsPage.results.artist[0].id;
+      search_similar_artist(artist_id);
 
-      });
+      up_coming_event(artist_id);
+      temp_id = artist_id;
+      console.log("inside .ajax "+temp_id);
 
+    })
+    .then(function() {
+        console.log("promise", artist_id)
+    });
+  
+   
 };
-
 
 //search by artist and data range  e.g. data format: min_date=2009-10-01 & max_date:2009-10-30
 
@@ -40,10 +53,9 @@ function search_by_artist_datarange(artist_name, min_date, max_date)
 }
 // search by similar artist
 //"https://api.songkick.com/api/3.0/artists/{artist_id}/similar_artists.json?apikey=4s4kGBz0WjJ4Yk6O"
-function search_similar_artist(artist_name){
-    var artist_name;
-
-    var queryURL="https://api.songkick.com/api/3.0/artists/"+artist_name+"/similar_artists.json?apikey=4s4kGBz0WjJ4Yk6O";
+function search_similar_artist(artist_id)
+{
+    var queryURL="https://api.songkick.com/api/3.0/artists/"+artist_id+"/similar_artists.json?apikey=4s4kGBz0WjJ4Yk6O";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -122,16 +134,32 @@ function search_by_venueName(venueName){
 
 function search_by_metroArea(metroName){
 
-    var queryURL ="https://api.songkick.com/api/3.0/metro_areas/{metro_area_id}/calendar.json?apikey={your_api_key}"
+    var queryURL ="https://api.songkick.com/api/3.0/metro_areas/{metro_area_id}/calendar.json?apikey=4s4kGBz0WjJ4Yk6O";
+}
+
+
+function up_coming_event(artist_id){
+
+    var queryURL = "https://api.songkick.com/api/3.0/artists/"+artist_id+"/calendar.json?apikey=4s4kGBz0WjJ4Yk6O";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+       
+      
+      });
+
 }
 
 //$(document).ready(function(){
 var band_name = "Mariah Carey";
-search_by_artist(band_name);
+artist_id= search_by_artist(band_name);
+
+console.log(" in the main  artist id  "+artist_id);
 
 //});
-
-//search_similar_artist(band_name);
 
 
 var venue_name="San Diego"
